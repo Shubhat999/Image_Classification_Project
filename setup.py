3d8b0ba75854b3e93c6c78ccfdeb1e977d70e65d
Shubhat999 <shubhat999@gmail.come>
2023-12-06 21:42:36 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages , setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(filepath:str)->List[str]:
+    requirements = []
+    with open(filepath) as f:
+        requirements = f.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+
+name='IMCPROJECT',
+version='0.0.1',
+author = 'Shubham',
+author_email='shubhat999@gmail.com',
+packages=find_packages(),
+install_requires = get_requirements('requirements.txt')
+
+)
+
+
